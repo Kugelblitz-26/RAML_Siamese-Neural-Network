f6111e67aae529ed59b565ad4eff7f78e11331f7
Prem Shinde <74344694+Kugelblitz-26@users.noreply.github.com>
2025-05-20 11:52:09 +0200

    Add files via upload

---
+++ src/model.py
@@ -0,0 +1,32 @@
+# src/model.py
+import torch
+import torch.nn as nn
+
+class SiameseNetwork(nn.Module):
+    def __init__(self, embedding_dim=128):
+        super(SiameseNetwork, self).__init__()
+        # Simple CNN backbone
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(64 * 4 * 4, 256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, embedding_dim)
+        )
+
+    def forward_once(self, x):
+        x = self.conv(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+    def forward(self, input1, input2):
+        out1 = self.forward_once(input1)
+        out2 = self.forward_once(input2)
+        return out1, out2
